c8dc6ea270f3ddc042dcddeb0310202452ed5af3
PNZBEIJINGL <beijing_l@sohu.com>
2019-05-14 00:10:08 +0800

    2019051322258

--- src/main/java/com/springsecurity/test/NoSecurityApplication.java
+++ src/main/java/com/springsecurity/test/NoSecurityApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * EnableAutoConfiguration 关闭springsecurity默认验证
+ */
 @SpringBootApplication(scanBasePackages = {"com"})
 @EnableAutoConfiguration(exclude = {
         org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
