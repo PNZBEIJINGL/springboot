ab7bad11f6b68e6b2aa1e6ac2943c876c47205d7
PNZBEIJINGL <beijing_l@sohu.com>
2019-05-14 23:58:30 +0800

    2019051422358

---
+++ src/main/java/com/springsecurity/test/MyUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.springsecurity.test;
+
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+
+public class MyUserDetailsService implements UserDetailsService {
+
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        logger.info("用户的用户名: {}", username);
+        // TODO 根据用户名，查找到对应的密码，与权限
+
+        // 封装用户信息，并返回。参数分别是：用户名，密码，用户权限
+        User user = new User(username, "123456",
+                AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
+        return user;
+    }
+}
