b3d498a0e762fc39307200912318769e0a2d4438
PNZBEIJINGL <beijing_l@sohu.com>
2019-05-15 23:56:51 +0800

    201905162350

--- src/main/java/com/springsecurity/test/package-info.java
+++ src/main/java/com/springsecurity/test/package-info.java
@@ -1,5 +1,7 @@
 package com.springsecurity.test;
 /*
- * 1. 运行NoSecurityApplication:注解不使用security验证
+ * 1. 运行NoSecurityApplication:注解不使用security验证,EnableAutoConfiguration可以关闭验证
+ * @EnableAutoConfiguration(exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
+ *
  * 2. https://www.jb51.net/article/140429.htm
  */
