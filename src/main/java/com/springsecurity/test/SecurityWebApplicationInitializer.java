9d917247ff83ded28106e76e977abc4cb60d0fb4
PNZBEIJINGL <yong620yong@sohu.com>
2019-05-15 09:08:17 +0800

    201905150900

--- src/main/java/com/springsecurity/test/SecurityWebApplicationInitializer.java
+++ src/main/java/com/springsecurity/test/SecurityWebApplicationInitializer.java
@@ -2,13 +2,13 @@ package com.springsecurity.test;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-
+//public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+public class SecurityWebApplicationInitializer{
     /**
      * 自动为应用程序中的每个URL注册springSecurityFilterChain过滤器
      * 添加一个加载WebSecurityConfig的ContextLoaderListener
      */
     public SecurityWebApplicationInitializer() {
-        super(WebSecurityConfig.class);
+        //super(WebSecurityConfig.class);
     }
 }
