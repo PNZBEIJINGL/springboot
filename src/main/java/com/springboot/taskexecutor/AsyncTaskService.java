82145369bf2072a2b8689b0b1bcc759fb248b2b8
PNZBEIJINGL <beijing_l@sohu.com>
2019-05-23 00:57:49 +0800

    201905230030

--- src/main/java/com/springboot/taskexecutor/AsyncTaskService.java
+++ src/main/java/com/springboot/taskexecutor/AsyncTaskService.java
@@ -9,6 +9,7 @@ public class AsyncTaskService {
     //@Async 注解表名该方法是一个异步方法，这里的方法自动被注入使用ThreadPoolTaskExecutor
     //如果注解在类级别，表示该类的所有方法都是异步方法
 
+
     @Async
     public void executeAsynTask(Integer interger) {
         System.out.println("执行异步任务 ： " + interger);
