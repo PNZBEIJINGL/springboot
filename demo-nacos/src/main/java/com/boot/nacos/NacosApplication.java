c5c871e4cffe3959eca6a2d696b535a34da4eb9d
liuyong <liuyong@startimes.com.cn>
2022-02-16 09:44:58 +0800

    02160944 Spring Boot + Nacos

--- demo-nacos/src/main/java/com/boot/nacos/NacosApplication.java
+++ demo-nacos/src/main/java/com/boot/nacos/NacosApplication.java
@@ -5,15 +5,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
+@RestController
 @SpringBootApplication
-@NacosPropertySource(dataId = "example", autoRefreshed = true)
-public class BasicApplication {
+//@NacosPropertySource(dataId = "nacos-demo.yaml", autoRefreshed = true)
+public class NacosApplication {
 
     public static void main(String[] args) {
         //调用委托给Spring Boot的run方法
         //SpringApplication引导启动spring,然后启动配置的tomcat容器
-        SpringApplication.run(BasicApplication.class, args);
+        SpringApplication.run(NacosApplication.class, args);
     }
 
     @GetMapping("/hello")
