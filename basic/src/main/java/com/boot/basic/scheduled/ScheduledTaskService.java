0aa90785e2c3920fbd9f8d1c5d5c905db7ea91c4
liuyong <liuyong@startimes.com.cn>
2022-02-22 18:54:19 +0800

    2202221853 Spring Boot调度

--- basic/src/main/java/com/boot/basic/scheduled/ScheduledTaskService.java
+++ basic/src/main/java/com/boot/basic/scheduled/ScheduledTaskService.java
@@ -1,12 +1,15 @@
 package com.boot.basic.scheduled;
 
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-@Service
+@Component
+@EnableScheduling
 public class ScheduledTaskService {
 
     private static final SimpleDateFormat dataFormat=new SimpleDateFormat("HH:mm:ss");
@@ -16,8 +19,10 @@ public class ScheduledTaskService {
         System.out.println("间隔5秒执行："+dataFormat.format(new Date()));
     }
 
-    @Scheduled(cron = "0/15 * * 17 * *")
+    @Scheduled(cron = "0/1 * * * * ?")
     public void fixTimeExecution(){
+        //@Scheduled 不支持7位,即不支持年
+        //异常: Cron expression must consist of 6 fields (found 7 in "0/1 * * * * ? 2099")
         System.out.println("指定时间执行："+dataFormat.format(new Date()));
     }
 
---
+++ basic/src/main/java/com/boot/basic/scheduled/SchedulerApplication.java
@@ -0,0 +1,14 @@
+package com.boot.basic.scheduled;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
+@EnableScheduling
+@SpringBootApplication
+public class SchedulerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulerApplication.class, args);
+    }
+}
